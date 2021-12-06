5cd66414d1da6e15c8ecd39aadc6dff82a5c5def
algolia-demo-nba <95610121+algolia-demo-nba@users.noreply.github.com>
2021-12-06 10:37:04 -0500

    Updated comments

--- src/app.js
+++ src/app.js
@@ -20,17 +20,21 @@ search.addWidgets([
     templates: {
       item: `
         <div>
+          // Add logo image based on logoUrl attribute to team module
           <img src={{logoUrl}} align="left" alt="{{name}}" width=150 height=125/>
           <div class="hit-description">
+          // Add team name attribute to team module
           Team Name:
            {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
          <br />
 
+// Add location attribute to team module
                      Location:
  {{#helpers.highlight}}{ "attribute": "location" }{{/helpers.highlight}}
          <br />
      Conference:
 
+ // Add number of championships attribute to team module
  {{#helpers.highlight}}{ "attribute": "conference" }{{/helpers.highlight}}
           <br />
           Championships:  {{#helpers.highlight}}{ "attribute": "Number of Championships" }{{/helpers.highlight}}
@@ -49,12 +53,14 @@ search.addWidgets([
 instantsearch.widgets.clearRefinements({
     container: '#clear-refinements',
   }),
-
+// Add conference as a refinement option to UI
   instantsearch.widgets.refinementList({
     container: '#conference-list',
     attribute: 'conference',
 
   }),
+// Add number of championships as a refinement option to UI
+
     instantsearch.widgets.refinementList({
     container: '#champs-list',
     attribute: 'Number of Championships',
